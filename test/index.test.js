f68d9c8b4525b94ac55ad9a1d3bee177ae761ece
kagawagao <kingsongao1221@gmail.com>
2017-12-04 20:32:01 +0800

    CHore: Add test

---
+++ test/index.test.js
@@ -0,0 +1,57 @@
+const test = require('ava')
+const filterString = require('../lib')
+
+test('Should be parsed rightly', t => {
+  // normally
+  const $filter1 = 'a eq xxx and b gt yyy'
+  const filters1 = filterString.parse($filter1)
+  t.deepEqual([{
+    key: 'a',
+    op: 'eq',
+    value: 'xxx'
+  }, {
+    key: 'b',
+    op: 'gt',
+    value: 'yyy'
+  }], filters1)
+
+  // some special
+  const $filter2 = 'a eq xxx zzz and b gt yyy'
+  const filters2 = filterString.parse($filter2)
+  t.deepEqual([{
+    key: 'a',
+    op: 'eq',
+    value: 'xxx zzz'
+  }, {
+    key: 'b',
+    op: 'gt',
+    value: 'yyy'
+  }], filters2)
+})
+
+test('Should be stringified rightly', t => {
+  // normally
+  const filters1 = [{
+    key: 'a',
+    op: 'eq',
+    value: 'xxx'
+  }, {
+    key: 'b',
+    op: 'gt',
+    value: 'yyy'
+  }]
+  const $filter1 = filterString.stringify(filters1)
+  t.is('a eq xxx and b gt yyy', $filter1)
+  // some special
+  const filters2 = [{
+    key: 'a',
+    op: 'eq',
+    value: 'xxx zzz'
+  }, {
+    key: 'b',
+    op: 'gt',
+    value: 'yyy'
+  }]
+  const $filter2 = filterString.stringify(filters2)
+  t.is('a eq xxx zzz and b gt yyy', $filter2)
+})
